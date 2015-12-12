7cf2869a7183b613ab3f4ef9647352a808eb3e28
sergiomrd <sergio.del93@gmail.com>
2015-12-12 07:38:20 +0100

    Started
    Setting up scenes

---
+++ Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+
+	void Awake()
+	{
+		if (instance != null) {
+
+			DestroyImmediate (gameObject);
+
+		}
+
+		instance = this;
+
+		DontDestroyOnLoad (gameObject);
+
+	}
+
+	public void ChangeLevel(string level)
+	{
+		switch (level) {
+
+		case "Kid":
+			EditorSceneManager.LoadScene ("Kid");
+			break;
+
+		case "Adult":
+			EditorSceneManager.LoadScene ("Adult");
+			break;
+
+		case "Elder":
+			EditorSceneManager.LoadScene ("Elder");
+			break;
+
+		}
+
+	}
+
+}
---
+++ Project/Assets/Scripts/OptionManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptionManager : MonoBehaviour {
+
+	public static OptionManager instance = null;
+
+	void Awake()
+	{
+		if (instance != null) {
+
+			DestroyImmediate (gameObject);
+
+		}
+
+		instance = this;
+
+		DontDestroyOnLoad (gameObject);
+
+	}
+
+
+
+
+}
